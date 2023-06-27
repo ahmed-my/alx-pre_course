5d5317103a02f9f14544443da1903c8d7a1b25bb
ahmed-my <yahaya.mymohammed@gmail.com>
2023-06-27 16:18:40 -0700

    python progam

---
+++ 0x01-git/sum.py
@@ -0,0 +1,10 @@
+# A program to sum the square of the first 10 natural numbe
+num = []
+print("Enter the numbers")
+for i in range[10]:
+    val = int(input())
+    num.append(val)
+sum = 0
+for i in range[10]:
+    sum = sum + (num[i] * num[i])
+print sum
